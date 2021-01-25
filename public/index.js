f9fd5eee41a6abe8876f4b0ebe230714c1768c06
MikeFranco <mfranco_98@yahoo.com>
2021-01-24 23:56:44 -0600

    added dynamic cards

--- public/index.js
+++ public/index.js
@@ -3,6 +3,8 @@ const logOut = document.querySelector('.logout');
 const card = document.querySelector('.card-list');
 const loggedIn = document.querySelectorAll('.logged-in');
 const loggedOut = document.querySelectorAll('.logged-out');
+const save = document.querySelector('.save-btn');
+const next = document.querySelector('.next-btn');
 
 const doLoginWithGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -34,56 +36,43 @@ logOut.addEventListener('click', e => {
     .catch(error => console.error(error));
 });
 
+const getCharacterCard = data => `
+  <div class="card-box">
+    <div class="box-header">
+      <span>Personaje</span>
+      <h1 id="name">${data.name}</h1>
+    </div>
+    <div class="box-body">
+      <div class="box-body-infos">
+        <ul>
+          <li>
+            <span>Altura</span>
+            <p id="population">${data.height}</p>
+          </li>
+          <li>
+            <span>Fecha de nacimiento</span>
+            <p id="climate">${data.birth_year}</p>
+          </li>
+          <li>
+            <span>Color de piel</span>
+            <p id="terrain">${data.skin_color}</p>
+          </li>
+          <li>
+            <span>Color de ojos</span>
+            <p id="gravity">${data.eye_color}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+`;
+
+let currentUserCharacter = 1;
 const setupCharacters = userData => {
   if (userData.length) {
     const html = userData.map(doc => {
       const data = doc.data();
-      return `
-      <div class="card-box">
-        <div class="box-header">
-          <span>Personaje</span>
-          <h1 id="name">${data.name}</h1>
-        </div>
-
-        <div class="box-body">
-          <div class="box-body-infos">
-            <ul>
-              <li>
-                <span>Altura</span>
-                <p id="population">${data.height}</p>
-              </li>
-              <li>
-                <span>Fecha de nacimiento</span>
-                <p id="climate">${data.birthday}</p>
-              </li>
-              <li>
-                <span>Color de piel</span>
-                <p id="terrain">${data.skin_color}</p>
-              </li>
-              <li>
-                <span>Color de ojos</span>
-                <p id="gravity">${data.eye_color}</p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6 next">
-            <button id="button" class="btn">
-              Save
-            </button>
-          </div>
-
-          <div class="col-sm-12 col-md-6 next">
-            <button id="button" class="btn">
-              Next
-            </button>
-          </div>
-        </div>
-      </div>
-      `;
+      return getCharacterCard(data);
     });
     card.innerHTML = html;
   } else {
@@ -111,4 +100,19 @@ auth.onAuthStateChanged(user => {
       ele.style.display = 'none';
     });
   }
+});
+
+let currentCharacter = 1;
+const getNextCharacter = async () => {
+  const resp = await fetch(`https://swapi.dev/api/people/${currentCharacter}`);
+  const data = await resp.json();
+  console.log(data);
+  currentCharacter++;
+  console.log('+++++++++++');
+  console.log(currentCharacter);
+  return getCharacterCard(data);
+};
+
+next.addEventListener('click', async(e) => {
+  card.innerHTML = await getNextCharacter();
 });
